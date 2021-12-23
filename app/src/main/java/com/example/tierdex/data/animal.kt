d9012099f19d430ca9dfc46e6661302ae3bb5e57
Julian Schiller <julian.schiller@smail.th-koeln.de>
2021-12-23 13:57:44 +0100

    data-package in com.example.tierdex angelegt und erste entity-class für animal angelegt

---
+++ app/src/main/java/com/example/tierdex/data/animal.kt
@@ -0,0 +1,12 @@
+package com.example.tierdex.data
+
+
+//This class will represent a database entity in our app
+class animal(
+    val anmialID: Int = 0,
+    val animalSpecies: String,
+    val animalFamily: String,
+    val animalOrder: String,
+    val animalClass: String
+) {
+}
