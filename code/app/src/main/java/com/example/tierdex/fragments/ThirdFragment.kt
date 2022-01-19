fd918bfeacf4c131b529f117c1284946ab2546ac
Cruv3 <andischurik@yahoo.de>
2022-01-19 20:04:44 +0100

    Added RecyclerView

--- code/app/src/main/java/com/example/tierdex/fragments/ThirdFragment.kt
+++ code/app/src/main/java/com/example/tierdex/fragments/ThirdFragment.kt
@@ -9,7 +9,6 @@ import com.example.tierdex.R
 
 class ThirdFragment : Fragment() {
 
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
