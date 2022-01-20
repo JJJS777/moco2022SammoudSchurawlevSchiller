0a50192864cf8567f8b8bcd79de93aff36092386
Julian Schiller <julian.schiller@smail.th-koeln.de>
2022-01-20 18:11:05 +0100

    komponenten in package gepackt, nach dem Codelab vorbild. Projekt build is fehlerfrei

--- code/app/src/main/java/com/example/tierdex/fragments/HomeFragment.kt
+++ code/app/src/main/java/com/example/tierdex/fragments/HomeFragment.kt
@@ -6,7 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.tierdex.databinding.FragmentHomeBinding
-import com.example.tierdex.model.DataSource
+import com.example.tierdex.data.DataSource
 import com.example.tierdex.model.ItemAdapter
 
 
