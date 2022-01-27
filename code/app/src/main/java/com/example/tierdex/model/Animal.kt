fb89b76d8557cca55bce86ea123ed11ef0039ee3
Julian Schiller <julian.schiller@smail.th-koeln.de>
2022-01-27 11:42:49 +0100

    Data Class Animal an Api angepasst

--- code/app/src/main/java/com/example/tierdex/model/Animal.kt
+++ code/app/src/main/java/com/example/tierdex/model/Animal.kt
@@ -1,8 +1,17 @@
 package com.example.tierdex.model
 
+import android.app.appsearch.SearchResults
 import com.squareup.moshi.Json
+
+data class response(
+    val searchResults: SearchResults
+)
+
+data class searchResults(
+    val results: List<Animal>
+)
 
 data class Animal(
     val id: String,
     // used to map img_src from the JSON to imgSrcUrl in our class
-    @Json(name = "img_src") val imgSrcUrl: String ) {}
+    @Json(name = "imageUrl") val imgSrcUrl: String ) {}
